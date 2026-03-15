b8c0683192a07da67e0859cfe99aee878d7b843d
若笛 <ludy111012@gmail.com>
2026-03-15 19:31:14 +0800

    style(theme): align root palette vars to requested brand tokens

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -8,7 +8,21 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ConfigProvider locale={zhCN}>
+    <ConfigProvider
+      locale={zhCN}
+      theme={{
+        token: {
+          colorPrimary: '#4ecdc4',
+          colorInfo: '#4ecdc4',
+          colorSuccess: '#45b7d1',
+          colorWarning: '#f9c80e',
+          colorError: '#ff7875',
+          colorBgBase: '#f7f9fc',
+          colorTextBase: '#2f3b4a',
+          borderRadius: 10
+        }
+      }}
+    >
       <BrowserRouter>
         <App />
       </BrowserRouter>
